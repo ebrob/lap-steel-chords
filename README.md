570b6b19cab2c966df68b8940c85173b8b74e0c7
Spamagnet <rob@scottclan.cc>
2024-09-16 14:07:19 -0400

    Minor UI fixes. Improved build.ps1. Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lap-steel-chords
+
+## To build
+
+Clone the https://github.com/eddievelasquez/Bach project into a "sister" folder next to this one.
+
+Run `build.ps1`
+
+It should now be runnable as a static HTML website.
--- build.ps1
+++ build.ps1
@@ -1,13 +1,17 @@
 pushd lap-steel-chord-generator
 # This project must be available: https://github.com/eddievelasquez/Bach
-Remove-Item .\Bach.Model\ -Recurse -Force
-#cp ..\..\Bach\src\Bach.Model .
+if (Test-Path .\Bach.Model\) {
+	Remove-Item .\Bach.Model\ -Recurse -Force
+}
 Copy-Item ..\..\Bach\src\Bach.Model -Destination . -Recurse
 pushd lap-steel-chord-generator
 dotnet run
 popd
 popd
+
 pushd lap-steel-chord-displayer
+New-Item -ItemType Directory -Force -Path ./scripts
+New-Item -ItemType Directory -Force -Path ./styles
 npm install
 cp ./node_modules/jquery/dist/jquery.slim.min.* ./scripts
 cp ./node_modules/svguitar/dist/svguitar.umd.* ./scripts
